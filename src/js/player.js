06d0d9252dfbdbbeea3a4a9b156c30049247de50
James McKay <james.mckay@trausteknik.com>
2020-12-12 11:39:19 +1100

    Change fullscreen button to change text to 'Restore' when in fullscreen

--- src/js/player.js
+++ src/js/player.js
@@ -125,6 +125,7 @@ apbp.playerIndex = 0;
         nextText: "Next Track",
         prevText: "Previous Track",
         fullscreenText: "Fullscreen",
+        fullscreenTextExit: "Restore",
 
         // Don't hide the previous image when showing the next one
         stacking: false,
@@ -1296,9 +1297,14 @@ apbp.playerIndex = 0;
                 };
                 player.globalBind(mejs.MediaFeatures.fullScreenEventName, func);
             }
-            t.fullscreenBtn = $('<span class="apbp-button apbp-fullscreen-expandcontract">' + '<button type="button" aria-controls="' + t.id + '" title="' + t.options.fullscreenText + '" aria-label="' + t.options.fullscreenText + '">'
-                    + '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 32 448 448"><path class="compress" d="M436 192H312c-13.3 0-24-10.7-24-24V44c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v84h84c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12zm-276-24V44c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v84H12c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h124c13.3 0 24-10.7 24-24zm0 300V344c0-13.3-10.7-24-24-24H12c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h84v84c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm192 0v-84h84c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12H312c-13.3 0-24 10.7-24 24v124c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12z"/><path class="expand" d="M0 180V56c0-13.3 10.7-24 24-24h124c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H64v84c0 6.6-5.4 12-12 12H12c-6.6 0-12-5.4-12-12zM288 44v40c0 6.6 5.4 12 12 12h84v84c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12V56c0-13.3-10.7-24-24-24H300c-6.6 0-12 5.4-12 12zm148 276h-40c-6.6 0-12 5.4-12 12v84h-84c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h124c13.3 0 24-10.7 24-24V332c0-6.6-5.4-12-12-12zM160 468v-40c0-6.6-5.4-12-12-12H64v-84c0-6.6-5.4-12-12-12H12c-6.6 0-12 5.4-12 12v124c0 13.3 10.7 24 24 24h124c6.6 0 12-5.4 12-12z"/></svg>'
-                    + '</button>' + "</span>");
+            t.fullscreenBtn = $('<span class="apbp-button apbp-fullscreen-expandcontract">'
+                    + '<button type="button" aria-controls="' + t.id + '" class="compress" title="' + t.options.fullscreenTextExit + '" aria-label="' + t.options.fullscreenTextExit + '">'
+                    + '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 32 448 448"><path class="compress" d="M436 192H312c-13.3 0-24-10.7-24-24V44c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v84h84c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12zm-276-24V44c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v84H12c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h124c13.3 0 24-10.7 24-24zm0 300V344c0-13.3-10.7-24-24-24H12c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h84v84c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm192 0v-84h84c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12H312c-13.3 0-24 10.7-24 24v124c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12z"/></svg>'
+                    + '</button>'
+                    + '<button type="button" aria-controls="' + t.id + '" class="expand" title="' + t.options.fullscreenText + '" aria-label="' + t.options.fullscreenText + '">'
+                    + '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 32 448 448"><path class="expand" d="M0 180V56c0-13.3 10.7-24 24-24h124c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H64v84c0 6.6-5.4 12-12 12H12c-6.6 0-12-5.4-12-12zM288 44v40c0 6.6 5.4 12 12 12h84v84c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12V56c0-13.3-10.7-24-24-24H300c-6.6 0-12 5.4-12 12zm148 276h-40c-6.6 0-12 5.4-12 12v84h-84c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h124c13.3 0 24-10.7 24-24V332c0-6.6-5.4-12-12-12zM160 468v-40c0-6.6-5.4-12-12-12H64v-84c0-6.6-5.4-12-12-12H12c-6.6 0-12 5.4-12 12v124c0 13.3 10.7 24 24 24h124c6.6 0 12-5.4 12-12z"/></svg>'
+                    + '</button>'
+                    + "</span>");
             var hasFullscreenFeature = false;
             console.log(player.options);
             console.log(player.options.features);
